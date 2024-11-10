a7e0f65827bbaa8b01cff1c7d5495b912953bc4d
Nguyễn Xuân Thịnh <nguyenxuanthinh345@gmail.com>
2024-11-10 17:35:25 +0700

    1

---
+++ be/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+app.use(cors()); // Sử dụng cors middleware
+
+app.post('/save-result', (req, res) => {
+  console.log('Received request:', req.body); // Log request body
+  const result = req.body;
+  const filePath = path.join(__dirname, 'result.json');
+
+  fs.writeFile(filePath, JSON.stringify(result, null, 2), (err) => {
+    if (err) {
+      console.error('Error saving file:', err); // Log error
+      return res.status(500).json({ message: 'Error saving file' });
+    }
+    console.log('File saved successfully'); // Log success
+    res.status(200).json({ message: 'File saved successfully' });
+  });
+});
+
+app.get('/result.json', (req, res) => {
+  const filePath = path.join(__dirname, 'result.json');
+  if (fs.existsSync(filePath)) {
+    res.sendFile(filePath);
+  } else {
+    res.status(404).json({ message: 'File not found' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
