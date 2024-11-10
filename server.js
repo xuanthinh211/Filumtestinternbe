b91315560f249eaf8b3d6e55ce3e3e7f6dab6a72
Nguyễn Xuân Thịnh <nguyenxuanthinh345@gmail.com>
2024-11-10 19:15:47 +0700

    1

--- server.js
+++ server.js
@@ -5,24 +5,29 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 5000; // Sử dụng PORT từ môi trường
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(cors({
-  origin: 'https://filumtestintern.vercel.app' // Chỉ cho phép truy cập từ URL frontend
+  origin: 'https://filumtestintern.vercel.app' // Cấu hình CORS cho phép frontend từ Vercel
 }));
 
+// Thêm endpoint xử lý GET tại '/'
+app.get('/', (req, res) => {
+  res.send('Backend is running!'); // Hoặc bạn có thể trả về HTML, JSON nếu cần
+});
+
 app.post('/save-result', (req, res) => {
-  console.log('Received request:', req.body); // Log request body
+  console.log('Received request:', req.body);
   const result = req.body;
   const filePath = path.join(__dirname, 'result.json');
 
   fs.writeFile(filePath, JSON.stringify(result, null, 2), (err) => {
     if (err) {
-      console.error('Error saving file:', err); // Log error
+      console.error('Error saving file:', err);
       return res.status(500).json({ message: 'Error saving file' });
     }
-    console.log('File saved successfully'); // Log success
+    console.log('File saved successfully');
     res.status(200).json({ message: 'File saved successfully' });
   });
 });
